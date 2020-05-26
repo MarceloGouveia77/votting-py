72598c2ad66e8ef86693997dc2ab831cecdb299c
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2020-05-26 01:45:47 -0300

    installing docker

--- votting/settings.py
+++ votting/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@oy@gw&wcobhn81bc(kjr91js9ttlvhp@^*#0yf0f026+5f*qr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1)) 
 
 ALLOWED_HOSTS = ['*']
 
